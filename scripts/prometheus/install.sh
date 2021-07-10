72f796fc4ae839c0ea952780ae3c4340cb1b3c40
Mayo <mayocream39@yahoo.co.jp>
2021-07-10 16:43:45 +0800

    fix install

--- scripts/prometheus/install.sh
+++ scripts/prometheus/install.sh
@@ -2,18 +2,9 @@
 
 set -eu
 
-# Make prometheus user
-sudo adduser --no-create-home --disabled-login --shell /bin/false --gecos "Prometheus Monitoring User" prometheus
-
 # Make directories and dummy files necessary for prometheus
-sudo mkdir /etc/prometheus
-sudo mkdir /var/lib/prometheus
-sudo touch /etc/prometheus/prometheus.yml
-sudo touch /etc/prometheus/prometheus.rules.yml
-
-# Assign ownership of the files above to prometheus user
-sudo chown -R prometheus:prometheus /etc/prometheus
-sudo chown prometheus:prometheus /var/lib/prometheus
+sudo mkdir -p /etc/prometheus | true
+sudo mkdir -p /var/lib/prometheus | true
 
 # Download prometheus and copy utilities to where they should be in the filesystem
 #VERSION=2.2.1
@@ -26,12 +17,6 @@ sudo cp prometheus-${VERSION}.linux-amd64/promtool /usr/local/bin/
 sudo cp -r prometheus-${VERSION}.linux-amd64/consoles /etc/prometheus
 sudo cp -r prometheus-${VERSION}.linux-amd64/console_libraries /etc/prometheus
 
-# Assign the ownership of the tools above to prometheus user
-sudo chown -R prometheus:prometheus /etc/prometheus/consoles
-sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
-sudo chown prometheus:prometheus /usr/local/bin/prometheus
-sudo chown prometheus:prometheus /usr/local/bin/promtool
-
 cat <<EOF | sudo tee /etc/systemd/system/prometheus.service
 [Unit]
 Description=Prometheus
@@ -39,8 +24,6 @@ Wants=network-online.target
 After=network-online.target
 
 [Service]
-User=prometheus
-Group=prometheus
 Type=simple
 ExecStart=/usr/local/bin/prometheus \
     --config.file /etc/prometheus/prometheus.yml \
