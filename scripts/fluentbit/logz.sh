1ab8b354d7ae06b15b7a5d6780b37dd333676304
Mayo <mayocream39@yahoo.co.jp>
2021-07-10 15:03:40 +0800

    logs metrics

--- scripts/fluentbit/logz.sh
+++ scripts/fluentbit/logz.sh
@@ -3,7 +3,7 @@
 set -eu
 
 wget -O /opt/td-agent-bit/bin/out_logzio.so \
-https://github.com/logzio/fluent-bit-logzio-output/blob/master/build/out_logzio-linux.so
+https://raw.githubusercontent.com/logzio/fluent-bit-logzio-output/master/build/out_logzio-linux.so
 
 cat <<EOF | tee /etc/td-agent-bit/plugins.conf
 [PLUGINS]
--- scripts/install-go.sh
+++ scripts/install-go.sh
@@ -4,6 +4,6 @@ set -eu
 
 wget https://golang.org/dl/go1.16.5.linux-amd64.tar.gz -O /tmp/go-release.tar.gz
 
-rm -rf /usr/local/go && tar -C /usr/local -xzf /tmp/go-release.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf /tmp/go-release.tar.gz
 
 echo "> export PATH=\$PATH:/usr/local/go/bin"
